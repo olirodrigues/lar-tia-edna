b61d350c3682c8aba0ee2afa98e2361a69d7def7
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-01-31 19:16:22 -0300

    alert removido

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ let enviarContato = document.getElementById("enviar-contato");
 enviarContato.addEventListener("click", (e) => {
   e.preventDefault();
   if (nome.value == "" || email.value == "" || mensagem.value == "") {
-    alert("Preencha todos os campos do formulário.");
   } else {
-    alert("Sucesso! Formulário enviado!");
     nome.value = "";
     email.value = "";
     mensagem.value = "";
