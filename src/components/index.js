10434265513593db517305f82c2afe461dead9f9
olirodrigues <olivia.rt@gmail.com>
2022-02-03 13:43:37 -0300

    adicionado botão flutuante na home

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+// funcionalidade do botão flutuante para voltar ao topo da pagina
+mybutton = document.getElementById("topBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
